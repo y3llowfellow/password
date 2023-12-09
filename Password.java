acfda1574bea82427a2666ee40c364a3f036760f
y3llowfellow <119467521+y3llowfellow@users.noreply.github.com>
2023-12-09 17:10:44 -0600

    Removed redundant comments and reformmated code

---
+++ Password.java
@@ -0,0 +1,22 @@
+import java.security.NoSuchAlgorithmException;
+//runs using command line
+public class Password {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        //user inputs plain text, SHA256, MD5, or BCrypt text
+        String password = args[0];
+
+        //user determines what technique to use. -bf is brute force, -cp is common passwords
+        String mode = args[1];
+
+        //user inputs that type of hash the password is. -p is plain text, -b is Bcrypt, -s is SHA256, -m is MD5.
+        String hashType = args[2];
+
+        if(mode.equals("-bf")){
+            (new BruteForce(password,hashType)).guessPassword();
+        }
+        else{
+            (new CommonPasswords(password,hashType)).guessPassword();
+        }
+
+    }
+}
