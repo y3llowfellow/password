89c6a35948f8ed59c91a6f1d6b7327853438b237
y3llowfellow <119467521+y3llowfellow@users.noreply.github.com>
2023-12-11 10:17:50 -0600

    Added Rainbow Table method

--- CommonPasswords.java
+++ CommonPasswords.java
@@ -1,7 +1,12 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.NoSuchAlgorithmException;
+import java.util.stream.Stream;
+
 import JBCrypt.BCrypt;
 
 public class CommonPasswords {
@@ -21,6 +26,62 @@ public class CommonPasswords {
             this.mode = "-p";
         }
     }
+    public void guessPasswordRT() throws IOException {
+        boolean found = false;
+        // search the MD5 hashes instead, if user inputs -m
+        if (mode.equals("-m")){
+            BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\colin\\Documents\\GitHub\\password\\10k-rainbowTable-MD5.txt"));
+            String line;
+            int lineNumber = 1;
+            //reading the line
+            while ((line = br.readLine()) != null && !found) {
+                line = line.substring(6);
+                //if the hashes match, then the word is found
+                if (line.equals(password)) {
+                    //print out the output
+                    System.out.println("Password found in the list of 10k most common passwords!");
+                    //print out corresponding line number of the plain text
+
+                    try (Stream<String> lines = Files.lines(Paths.get("C:\\Users\\colin\\Documents\\GitHub\\password\\10-million-password-list-top-10000.txt"))) {
+                        System.out.println(lines.skip((lineNumber+1)/2).findFirst().get());
+                    }
+                    lineNumber++;
+                    found = true;
+                }
+
+            }
+        }
+        else if(mode.equals("-s")){
+            BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\colin\\Documents\\GitHub\\password\\10k-rainbowTable-SHA256.txt"));
+            String line;
+            int lineNumber = 1;
+            //reading the line
+            while ((line = br.readLine()) != null && !found) {
+                line = line.substring(6);
+                //if the hashes match, then the word is found
+                if (line.equals(password)) {
+                    //print out the output
+                    System.out.println("Password found in the list of 10k most common passwords!");
+                    //print out corresponding line number of the plain text
+
+                    try (Stream<String> lines = Files.lines(Paths.get("C:\\Users\\colin\\Documents\\GitHub\\password\\10-million-password-list-top-10000.txt"))) {
+                        System.out.println(lines.skip((lineNumber)/2).findFirst().get());
+                    }
+                    lineNumber++;
+                    found = true;
+                }
+
+            }
+        } else{
+            System.out.println("Invalid mode entered. Rainbow Table can only be used for MD5 and SHA256 passwords");
+        }
+
+        //if password not found, then print out to indicate
+        if (!found) {
+            System.out.println("Password not found in 10k common datalist");
+        }
+
+    }
 
     public void guessPassword() throws NoSuchAlgorithmException {
         boolean found = false;
@@ -29,11 +90,12 @@ public class CommonPasswords {
             String line;
             //reading the line
             while ((line = br.readLine()) != null && !found) {
-                //Convert line to MD5 Hash
+                //Convert line to MD5 Hash if user inputed -m
                 switch (mode) {
                     case "-m": {
                         String hashedLine = Hasher.MD5(line);
                         if (hashedLine.equals(password)) {
+                            //print out output and corresponding plain text
                             System.out.println("Password found in the list of 10k most common passwords!");
                             System.out.println(line);
                             found = true;
@@ -43,6 +105,7 @@ public class CommonPasswords {
                     //Check if line matches BCrypt Hash
                     case "-b":{
                         if (BCrypt.checkpw(line, password)){
+                            //print out output and corresponding plain text
                             System.out.println("Password found in the list of 10k most common passwords!");
                             System.out.println(line);
                             found = true;
@@ -54,6 +117,7 @@ public class CommonPasswords {
                     case "-s": {
                         String hashedLine = Hasher.SHA256(line);
                         if (hashedLine.equals(password)) {
+                            //print out output and corresponding plain text
                             System.out.println("Password found in the list of 10k most common passwords!");
                             System.out.println(line);
                             found = true;
@@ -63,6 +127,7 @@ public class CommonPasswords {
                     //Keep the line as plain text and search
                     default:
                         if (line.equals(password)) {
+                            //print out output and corresponding plain text
                             System.out.println("Password found in the list of 10k most common passwords!");
                             System.out.println(line);
                             found = true;
--- Password.java
+++ Password.java
@@ -1,7 +1,8 @@
+import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 //runs using command line
 public class Password {
-    public static void main(String[] args) throws NoSuchAlgorithmException {
+    public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
         //user inputs plain text, SHA256, MD5, or BCrypt text
         String password = args[0];
 
@@ -11,11 +12,21 @@ public class Password {
         //user inputs that type of hash the password is. -p is plain text, -b is Bcrypt, -s is SHA256, -m is MD5.
         String hashType = args[2];
 
+        boolean useRainbowTable = args.length == 4 && args[3].equals("-y");
+
+
         if(mode.equals("-bf")){
             (new BruteForce(password,hashType)).guessPassword();
         }
         else{
-            (new CommonPasswords(password,hashType)).guessPassword();
+            //if user wants to use RainBow table
+            if(useRainbowTable){
+                (new CommonPasswords(password, hashType)).guessPasswordRT();
+            }
+            //user doesn't use rainbow table
+            else {
+                (new CommonPasswords(password, hashType)).guessPassword();
+            }
         }
 
     }
